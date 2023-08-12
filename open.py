5b7695a50d45c0f110866de92da3c2498fefa0d3
Nguyễn Quang Huy <huynq@isi.com.vn>
2023-08-12 14:35:44 +0700

    Update open.py

--- open.py
+++ open.py
@@ -11,15 +11,24 @@ df['Date'] = pd.to_datetime(df['Date'])
 df.sort_values('Date', inplace=True)
 
 # Xác định dữ liệu train và test
-train_data = df['Open'].values[:-1]  # Sử dụng tất cả dữ liệu trừ dòng cuối cùng làm dữ liệu train
-test_data = df['Open'].values[-1:]  # Dòng cuối cùng làm dữ liệu test
+train_data = df[:-1]  # Sử dụng tất cả dữ liệu trừ dòng cuối cùng làm dữ liệu train
+test_data = df[-1:]  # Dòng cuối cùng làm dữ liệu test
 
-# Xây dựng mô hình ARIMA
-model = ARIMA(train_data, order=(2, 1, 2))
-model_fit = model.fit()
+# Dự đoán giá trị 'open', 'close', 'high', 'low' cho 'date' tiếp theo
+forecast_data = []
+columns = ['Open', 'Close', 'High', 'Low']
 
-# Dự đoán giá trị 'open' cho 'date' tiếp theo
-forecast = model_fit.forecast(steps=1)[0]
+for column in columns:
+    # Xây dựng mô hình ARIMA cho cột dữ liệu hiện tại
+    model = ARIMA(train_data[column].values, order=(2, 1, 2))
+    model_fit = model.fit()
+
+    # Dự đoán giá trị cho 'date' tiếp theo
+    forecast = model_fit.forecast(steps=1)[0]
+
+    # Lưu trữ kết quả dự đoán
+    forecast_data.append(forecast)
 
 # In kết quả dự đoán
-print("Dự đoán giá trị 'open' cho 'date' tiếp theo:", forecast)
+for i, column in enumerate(columns):
+    print(f"Dự đoán giá trị '{column}' cho 'date' tiếp theo:", forecast_data[i])
