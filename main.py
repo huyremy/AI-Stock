4a70c7a8164bb186af791979c207a5c4ddf233b4
Nguyễn Quang Huy <huynq@isi.com.vn>
2023-08-13 05:52:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
 df = pd.read_csv('2019.csv', parse_dates=['Date'], index_col='Date')
@@ -8,6 +7,6 @@ df = pd.read_csv('2019.csv', parse_dates=['Date'], index_col='Date')
 model = ARIMA(df, order=(2, 1, 2))
 model_fit = model.fit()
 
-forecast = model_fit.forecast(steps=6)
+forecast = model_fit.forecast(steps=1)
 
 print(forecast)
